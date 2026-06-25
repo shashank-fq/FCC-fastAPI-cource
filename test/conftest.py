3c06d6d7a9e8f55367751f59cbeaaa50331e3df8
Shashank Shankar <clg.shashank@gmail.com>
2026-06-26 03:07:00 +0530

    added complete test for post endpoints

--- test/conftest.py
+++ test/conftest.py
@@ -10,6 +10,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 from app.database import get_db
 from app.database import Base
+from app.oauth2 import create_access_token
+from app import models
 # from alembic import command
 
 SQLALCHIME_DATABSE_URL = 'postgresql://postgres:dhisdat@localhost:5432/test_fastapi'
@@ -90,3 +92,60 @@ def test_user(client):
     new_user = res.json()
     new_user['password'] = user_data['password']
     return new_user
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {"email": "hello1234@gmail.com", "password": "password1234"}
+    res =client.post("/users/", json= user_data)
+    assert res.status_code == 201
+    # print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+    return client
+
+
+
+@pytest.fixture
+def test_posts(test_user, session, test_user2):
+    posts_data = [{
+        "title": "1st title",
+        "content": "1st content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    },{
+        "title": "4th title",
+        "content": "4th content",
+        "owner_id": test_user2['id']
+    }]
+    def create_post_model(post):
+        return models.Post(**post)
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+    session.add_all(posts)
+    # session.add_all([models.Post(title = "1st user", content = "1st content", owner_id = test_user['id']),
+    #                  models.Post(title = "2nd user", content = "2nd content", owner_id = test_user['id']),
+    #                  models.Post(title = "3rd user", content = "3rd content", owner_id = test_user['id'])])
+    session.commit()
+
+    posts = session.query(models.Post).all()
+    return posts
