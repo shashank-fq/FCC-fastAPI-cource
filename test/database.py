4f2134f7a52ca5db80bbb9e43d3951c3c0fe1c34
Shashank Shankar <clg.shashank@gmail.com>
2026-06-25 19:47:57 +0530

    compleated login user test

---
+++ test/database.py
@@ -0,0 +1,79 @@
+from fastapi.testclient import TestClient
+from app.main import app
+import pytest
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
+from app.database import get_db
+from app.database import Base
+# from alembic import command
+
+SQLALCHIME_DATABSE_URL = 'postgresql://postgres:dhisdat@localhost:5432/test_fastapi'
+
+
+# SQLALCHIME_DATABSE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/test_{settings.database_name}"
+
+engine = create_engine(SQLALCHIME_DATABSE_URL)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base.metadata.create_all(bind=engine)
+
+# Base = declarative_base()
+
+# def override_get_db():
+#     db = TestingSessionLocal()
+
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+# app.dependency_overrides[get_db] = override_get_db
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# client = TestClient(app)
+@pytest.fixture()
+def client(session):
+    # # when test finises the tables are droped
+    # # for new test new tables are created(old)
+
+    # #now if error occurs we can see db 
+    # Base.metadata.drop_all(bind=engine)
+
+    # run our code before we run our test
+    # Base.metadata.create_all(bind=engine)//moved to session
+
+    # command.upgrade("head")
+
+    # yield TestClient(app)
+
+    # command.downgrade("base")
+    # run our code after our test finishes
+
+
+
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
--- test/test_users.py
+++ test/test_users.py
@@ -1,17 +1,40 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas 
+import pytest
+from .database import client, session
+import jwt
+from app.config import settings
+    
+# def test_root(client):
+#     res = client.get("/")
+#     assert res.json().get("message") == "hello fastAPI"
+#     assert res.status_code == 200
 
-client = TestClient(app)
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "hello123@gmail.com", "password": "password123"}
+    res =client.post("/users/", json= user_data)
+    assert res.status_code == 201
+    print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
-def test_root():
-    res = client.get('/')
-    assert res.json().get('message') == 'hello fastAPI'
-    assert res.status_code == 200
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users/", json = {"email": "hello123@gmail.com", "password": "password123"})
     # print(res.json())
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
+
+
+def test_login_user(client, test_user):
+    res = client.post("/login", data = {"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms = [settings.algorithm])
+    id = payload.get("user_id")
+
+    assert id == test_user['id']
+    # print(res.json())
+    assert login_res.token_type =="bearer"
+    assert res.status_code == 200
